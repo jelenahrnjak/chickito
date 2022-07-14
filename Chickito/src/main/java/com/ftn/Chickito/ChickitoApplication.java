d1f7414fd851430e857955771f2632a94871d5bf
jelenahrnjak <jelenahrnjak99@gmail.com>
2022-07-14 22:26:48 +0200

    initial project

---
+++ Chickito/src/main/java/com/ftn/Chickito/ChickitoApplication.java
@@ -0,0 +1,13 @@
+package com.ftn.Chickito;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChickitoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChickitoApplication.class, args);
+	}
+
+}
